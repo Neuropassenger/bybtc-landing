9c9b8c3f52dfd1422c41973581e1908039bd8a15
Turgenoid <turgenoid@gmail.com>
2018-01-06 23:25:12 +0300

    Добавлена интерактивность для секции с каталогом

--- src/js/common.js
+++ src/js/common.js
@@ -6,7 +6,34 @@ $(document).ready(function(){
   	$('.mobile-menu').toggleClass('mobile-menu-active');
   });
 
-  $('.owl-carousel').owlCarousel({
-  	items: 1
+  // CATALOG 
+  var carousel = $('.owl-carousel');
+  var cat_number;
+  var cat_name
+
+  carousel.owlCarousel({
+  	items: 1,
+  	onInitialized: setCatName,
+  	onChanged: setCatName
   });
+
+  function setCatName(event) {
+  	cat_number = event.item.index;
+  	cat_name = $('.fullwidth-menu ul li:eq(' + cat_number + ') a').html();
+  	$('.content > h2').html(cat_name);
+
+  	$('.fullwidth-menu ul li:eq(' + cat_number + ') a').toggleClass('accent-text-color'); 
+
+  	for(var i = 0; i < event.item.count; i++) {
+  		if(i !== cat_number) {
+  			cat_name = $('.fullwidth-menu ul li:eq(' + i + ') a').removeClass();
+  		}
+  	}
+  }
+
+ 	$('.fullwidth-menu ul li').click(function(event) {
+ 		var cat_number = $('.fullwidth-menu ul li').index('.fullwidth-menu ul');
+  	console.log(cat_number);
+  	//$()
+ 	});
 });
